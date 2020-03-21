4eb4223c84a19707ab4ff9c77def1a6e9face72f
vagrant <vagrant@localhost.localdomain>
2020-03-21 06:09:08 +0000

    [Add]login

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,34 @@
+class UsersController < ApplicationController
+  def new
+  	@user = User.new
+  end
+
+  def create
+  	@user = User.new(user_params)
+  	if @user.save
+  		redirect_to admin_user_url(@user)
+  	else
+  		render :new
+  	end
+  end
+
+  def show
+  	@user = User.find(params[:id])
+  	@post_images = @user.post_images.page(params[:page]).reverse_order
+  end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
+  end
+
+private
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :profile_image)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  devise_for :users
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'books#top'
   resources 'books'
+
+  devise_for :users
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
