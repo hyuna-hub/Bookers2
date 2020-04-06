72b11c28839e9133de4bf56ce4bdb47c29652fa2
vagrant <vagrant@localhost.localdomain>
2020-04-06 14:56:28 +0000

    [Add]コメント機能

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,7 @@ Rails.application.routes.draw do
 # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   devise_for :users
   resources :users
+
+  resources :book_comments, only: [:create, :destroy]
+
 end
